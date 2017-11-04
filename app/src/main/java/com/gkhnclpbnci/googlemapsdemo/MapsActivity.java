e51c429ec17b860df413035a10b2373c72483145
gkhnclpbnci <gkhnclpbnci@gmail.com>
2017-11-04 15:33:48 +0300

    Harita metotları girildi

--- app/src/main/java/com/gkhnclpbnci/googlemapsdemo/MapsActivity.java
+++ app/src/main/java/com/gkhnclpbnci/googlemapsdemo/MapsActivity.java
@@ -1,23 +1,37 @@
 package com.gkhnclpbnci.googlemapsdemo;
 
-import android.support.v4.app.FragmentActivity;
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentActivity;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends FragmentActivity implements LocationListener,OnMapReadyCallback {
+
+    private static final float ZOOM_RATE = 15.0f ;
+    private double lat = -1, lng = -1;
 
     private GoogleMap mMap;
+    private LocationManager locationManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+
+        startLocationManager();
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
@@ -39,8 +53,112 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+     //   LatLng sydney = new LatLng(-34, 151);
+      //  mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+       // mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        mMap.setMyLocationEnabled(true);
+
+        Location location = getLastLocation();
+
+        float errorRate = location.getAccuracy();
+
+        Toast.makeText(this, "Hata payı : "+errorRate, Toast.LENGTH_SHORT).show();
+
+        setMapCamera(location);
+
+    }
+    private void setMapCamera(Location location) {
+
+        lat = location.getLatitude();
+
+
+        lng = location.getLongitude();
+
+        Toast.makeText(this, "lat:"+ lat, Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "lng:"+ lng, Toast.LENGTH_SHORT).show();
+//        Location myLocation = googleMap.getMyLocation();
+        LatLng currentLatLng = new LatLng(lat, lng);
+//        LatLng currentLatLng = new LatLng(myLocation.getLatitude(),myLocation.getLongitude());
+
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(currentLatLng, 15.0f));
+        mMap.animateCamera(CameraUpdateFactory.zoomTo(ZOOM_RATE), 2000, null);
+
+    }
+
+    private Location getLastLocation() {
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return null;
+        }
+        LocationManager locationManager = (LocationManager)
+                getSystemService(Context.LOCATION_SERVICE);
+        Criteria criteria = new Criteria();
+
+       Location location = locationManager.getLastKnownLocation(locationManager
+               .getBestProvider(criteria, false));
+        double latitude = location.getLatitude();
+       double longitude = location.getLongitude();
+
+     //   return locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+       return location;
+
+
+    }
+    private void startLocationManager() {
+
+        //TODO fix permission control
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+
+
+            return;
+        }
+
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
+                2000,
+                10,  this);
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+        setMapCamera(location);
+
+    }
+
+    @Override
+    public void onStatusChanged(String s, int i, Bundle bundle) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String s) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String s) {
+
     }
 }
